d89fa1cd7cf5e7d60ee0a6b5b8e91ff98b722013
乌龙茶 <982474256@qq.com>
2023-05-24 14:10:00 +0800

    原创深色模式

--- src/.vuepress/components/original/index.vue
+++ src/.vuepress/components/original/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <span class="nl-tag nl-tag-original">原创</span>
+  <span data-theme="light" id="nlOriginal" class="nl-original-wrapper nl-original">原创</span>
 </template>
-<style lang="scss" scoped>
-@import "./tag.scss";
+<style lang="scss">
+@import "./original.scss";
 </style>
