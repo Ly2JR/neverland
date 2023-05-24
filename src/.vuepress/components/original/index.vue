c406ea6a3c10eacc7191963d86d6b0681a0d7f16
乌龙茶 <982474256@qq.com>
2023-05-24 17:09:14 +0800

    修复原创高度异常的问题

--- src/.vuepress/components/original/index.vue
+++ src/.vuepress/components/original/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <span data-theme="light" id="nlOriginal" class="nl-original-wrapper nl-original">原创</span>
+  <span data-theme="light" class="nl-original-wrap nl-original">原创</span>
 </template>
 <style lang="scss">
 @import "./original.scss";
