1fb535280f465a965daf4818b003892844c79f0e
乌龙茶 <982474256@qq.com>
2023-07-21 13:59:51 +0800

    docs: 添加百度统计

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,17 +1,37 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
-import { getDirname, path } from "@vuepress/utils"
+import { getDirname, path } from "@vuepress/utils";
 
-const __dirname=getDirname(import.meta.url);
+const __dirname = getDirname(import.meta.url);
 
 /**
  * 替换原来的原创
  */
-const OriginalInfo=path.resolve(__dirname,"./components/original/index.ts")
+const OriginalInfo = path.resolve(__dirname, "./components/original/index.ts");
 
 export default defineUserConfig({
   base: "/",
-  lang:'zh-CN',
+  lang: "zh-CN",
+  head: [
+    [
+      "script",
+      {},
+      `
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?e0f4e638f3c9bb2b41419a3b473edbc4";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+
+      var hm1 = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?481113a321ae3a0c070f99fdffba4319";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm1, s);
+    })();
+    `,
+    ],
+  ],
   locales: {
     "/": {
       lang: "zh-CN",
@@ -22,10 +42,10 @@ export default defineUserConfig({
 
   theme,
 
-  alias:{
-    "@theme-hope/modules/info/components/OriginalInfo":OriginalInfo
+  alias: {
+    "@theme-hope/modules/info/components/OriginalInfo": OriginalInfo,
   },
 
   // Enable it with pwa
-   shouldPrefetch: false,
+  shouldPrefetch: false,
 });
